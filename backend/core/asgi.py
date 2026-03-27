0cd91e538e737c863c3d340657ebcc81575048aa
Mohamed Abed <momo55265526@gmail.com>
2026-03-28 00:03:59 +0200

    Fix ASGI AppRegistryNotReady error

--- backend/core/asgi.py
+++ backend/core/asgi.py
@@ -1,14 +1,17 @@
 import os
 from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
 from core.middleware import TokenAuthMiddleware
 from orders.routing import websocket_urlpatterns
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-
-django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
@@ -20,4 +23,3 @@ application = ProtocolTypeRouter({
         )
     ),
 })
-
